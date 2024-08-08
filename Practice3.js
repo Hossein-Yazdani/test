062ee468f51d6de50f90586763c3af0fee13fa06
unknown <yazdanih80.ce@gmail.com>
2024-08-08 20:02:28 +0330

    third js practice

---
+++ Practice3.js
@@ -0,0 +1,5 @@
+// var firstNumber = +prompt("Please Enter First Number :")
+// var secondNumber = +prompt("Please Enter Second Number :")
+// var thirdNumber = +prompt("Please Enter Third Number :")
+// var Average = (firstNumber+secondNumber+thirdNumber)/3
+// alert(Average)
