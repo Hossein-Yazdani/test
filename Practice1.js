5e08b329cdc35ea38f2d2e390838f40aa714362d
unknown <yazdanih80.ce@gmail.com>
2024-08-08 19:53:10 +0330

    First js practice

---
+++ Practice1.js
@@ -0,0 +1,9 @@
+// var userFirstNumber = +prompt("Please Enter First Number : ");
+// var userSecondNumber = +prompt("Please Enter Second Number : ");
+// var plus = userFirstNumber + userSecondNumber;
+// var mines = userFirstNumber - userSecondNumber;
+// var div = userFirstNumber / userSecondNumber;
+// var multi = userFirstNumber * userSecondNumber;
+// var Tavan = userFirstNumber ** userSecondNumber;
+// var Bagimande = userFirstNumber % userSecondNumber;
+// alert(plus);
