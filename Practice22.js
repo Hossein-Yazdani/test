0dae64eacd88afff1deb78f5febc16779f7bd359
Hossein-Yazdani <157222116+Hossein-Yazdani@users.noreply.github.com>
2024-08-09 22:58:27 +0330

    Update Practice22.js

--- Practice22.js
+++ Practice22.js
@@ -1,8 +1,9 @@
-// var userNumberAmount = +prompt("How many number to you want to calculate : ")
-// var userNumberAmount2 = userNumberAmount
-// var userNumber = null
-// while(userNumberAmount > 0){
-//     userNumber = userNumber + Number(prompt("Enter your numbers : "))
-//     userNumberAmount--
-// }
-// console.log(userNumber/userNumberAmount2)
+var userNumberAmount = +prompt("How many number to you want to calculate : ")
+var userNumberAmount2 = userNumberAmount
+var userNumber = null
+while(userNumberAmount > 0){
+     userNumber = userNumber + Number(prompt("Enter your numbers : "))
+     userNumberAmount--
+ }
+ console.log(userNumber/userNumberAmount2)
+hello js
