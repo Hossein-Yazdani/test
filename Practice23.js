1f8fc6d676c3b9b1bfc26af68da4c4e7ef6365a7
unknown <yazdanih80.ce@gmail.com>
2024-08-10 11:01:47 +0330

    new js practice (array)

---
+++ Practice23.js
@@ -0,0 +1,12 @@
+var userNumberCount = +prompt("How many numbers do you want to use : ");
+var userNumberCounter = userNumberCount
+var userNumbers = null;
+var userNumbersPlaceholder = [];
+var userNumbersPlaceholderSum = null
+for (i=0 ; userNumberCount > 0 ; i++) {
+  userNumbers = +prompt("Please enter Number " + userNumberCount + " : ");
+  userNumberCount--;
+  userNumbersPlaceholder.push(userNumbers);
+  userNumbersPlaceholderSum = userNumbersPlaceholderSum + userNumbersPlaceholder[i]
+}
+console.log("Sum : "+ userNumbersPlaceholderSum + "\n" +"Average : " + userNumbersPlaceholderSum/userNumberCounter)
